eb21a1eae860c8d7c1cb96ba918e910439a65ba1
danggeuni <dydrnxkeh@naver.com>
2023-02-20 17:59:59 +0900

    1

--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -94,6 +94,43 @@ export default function MainContents() {
                 </div>
               </div>
             </div>
+            <form className={styles.task_editor}>
+              <div className={styles.task_editor_editing_area}>
+                <div className={styles.task_editor_input_fields}>
+                  <div className={styles.task_editor_contents_field}>
+                    <div className={styles.task_editor_contents_name}>
+                      <input
+                        className={styles.task_editor_contents_name_input}
+                        placeholder={"작업 이름"}
+                      ></input>
+                      <input
+                        className={styles.task_editor_contents_desc_input}
+                        placeholder={"설명"}
+                      ></input>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.task_editor_button_area}>
+                  <div className={styles.seperate}>
+                    <div className={styles.due_date}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        className={styles.no_due_date}
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M12 2a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V4a2 2 0 012-2h8zm0 1H4a1 1 0 00-1 1v8a1 1 0 001 1h8a1 1 0 001-1V4a1 1 0 00-1-1zm-1.25 7a.75.75 0 110 1.5.75.75 0 010-1.5zm.75-5a.5.5 0 110 1h-7a.5.5 0 010-1h7z"
+                        ></path>
+                      </svg>
+                      <div>마감 날짜</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
