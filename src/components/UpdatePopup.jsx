59dfcce2e9c6b158603068437b471910e4497227
danggeuni <102232204+danggeuni@users.noreply.github.com>
2023-04-23 21:20:27 +0900

    1

--- src/components/UpdatePopup.jsx
+++ src/components/UpdatePopup.jsx
@@ -3,11 +3,36 @@ import { useGlobalContext } from "../context";
 import { useEffect } from "react";
 
 export default function UpdatePopup() {
-  const { showPopup, setShowPopup, data } = useGlobalContext();
+  const { showPopup, setShowPopup, data, control } = useGlobalContext();
 
   useEffect(() => {
-    //
-    setShowPopup(!showPopup);
+    const originLocalData = [...control];
+
+    const originTodayData = originLocalData.filter(
+      (item) => new Date(item.date) <= new Date()
+    ).length;
+
+    const originNextData = originLocalData.filter(
+      (item) => new Date(item.date) > new Date()
+    ).length;
+
+    const localData = localStorage.getItem("todo");
+    const copyData = JSON.parse(localData);
+
+    const newTodayData = copyData.filter(
+      (item) => new Date(item.date) <= new Date()
+    ).length;
+
+    const newNextData = copyData.filter(
+      (item) => new Date(item.date) > new Date()
+    );
+
+    if (newTodayData > originTodayData) {
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 5000);
+    }
   }, [data]);
 
   return (
