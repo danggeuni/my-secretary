aa5dedc787d8b4a1439624b5b95d19d325bccfa1
danggeuni <dydrnxkeh@naver.com>
2023-08-17 16:04:17 +0900

    1

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +20,5 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
-app.use(cors());
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
